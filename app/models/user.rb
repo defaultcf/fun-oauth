ba98a5b6e93888708b1938a0234ab04a3b81c438
i544c <b1015078@fun.ac.jp>
2019-06-11 18:21:39 +0900

    Add doorkeeper

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,13 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
          :confirmable
+
+  has_many :access_grants,
+           class_name: 'Doorkeeper::AccessGrant',
+           foreign_key: :resource_owner_id,
+           dependent: :delete_all
+  has_many :access_token,
+           class_name: 'Doorkeeper::AccessToken',
+           foreign_key: :resource_owner_id,
+           dependent: :delete_all
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   mount LetterOpenerWeb::Engine, at: '/letter_opener' if Rails.env.development?
   devise_for :users
+  use_doorkeeper
   root to: 'page#top'
 end
